1297f12c458e9e94b84c1f98a8a70247b349d069
Kishore S <ks6700822@gmail.com>
2026-09-19 22:09:16 +0530

    Solve Best Time to Buy and Sell Stock (Easy) - best-time-to-buy-and-sell-stock

---
+++ LeetCode/Easy/best-time-to-buy-and-sell-stock/Solution.java
@@ -0,0 +1,15 @@
+                minval = prices[i];
+            }
+            else{
+                int profit =  prices[i] - minval;
+                if(profit > maxval){
+            if(prices[i]<minval){
+        for(int i=0;i<prices.length;i++){
+
+        int minval = Integer.MAX_VALUE;
+        int maxval = 0;
+                    maxval = profit;
+                }
+            }
+        }
+          return maxval;
