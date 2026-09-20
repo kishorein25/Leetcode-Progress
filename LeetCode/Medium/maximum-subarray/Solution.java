5ed6ec1f61f56d55ffc5ce545b0b6dbae38c8eb2
Kishore S <ks6700822@gmail.com>
2026-09-20 21:45:13 +0530

    Solve Maximum Subarray (Medium) - maximum-subarray

---
+++ LeetCode/Medium/maximum-subarray/Solution.java
@@ -0,0 +1,16 @@
+                cursum = temp;
+            }
+
+            if(cursum > maxsum){
+                maxsum = cursum;
+            }
+            }else{
+                cursum = nums[i];
+            if(temp < nums[i]){
+            int temp = cursum + nums[i];
+
+        for(int i=0;i<nums.length;i++){
+        }
+        return maxsum;
+    }
+}
