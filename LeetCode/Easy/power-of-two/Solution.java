f438f3565ac34eab1460126af508778944404f97
Kishore S <ks6700822@gmail.com>
2026-09-22 22:08:20 +0530

    Solve Power of Two (Easy) - power-of-two

--- LeetCode/Easy/power-of-two/Solution.java
+++ LeetCode/Easy/power-of-two/Solution.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==1) return true;
+        return (n>0 && (n&(n-1))==0);
     }
-
-        else if(n%2==0) return true;
-
-        else return false;
 }
