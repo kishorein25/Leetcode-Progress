bfdf6911169a0bf79d99c4f6c9b17742099d2a67
Kishore S <ks6700822@gmail.com>
2026-09-24 23:19:53 +0530

    Solve Trapping Rain Water (Hard) - trapping-rain-water

---
+++ LeetCode/Hard/trapping-rain-water/Solution.java
@@ -0,0 +1,10 @@
+                if (height[left] >= leftM) {
+                    leftM = height[left];
+                } else {
+                    water = water + (leftM - height[left]);
+                }
+
+            if (height[left] < height[right]) {
+        while (left < right) {
+
+        int water = 0;
