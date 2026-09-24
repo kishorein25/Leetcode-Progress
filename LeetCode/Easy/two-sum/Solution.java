80036a669cb08294842a316af55bee6cd2d658a6
Kishore S <ks6700822@gmail.com>
2026-09-24 23:26:30 +0530

    Solve Two Sum (Easy) - two-sum

--- LeetCode/Easy/two-sum/Solution.java
+++ LeetCode/Easy/two-sum/Solution.java
@@ -2,8 +2,11 @@ class Solution {
     public int[] twoSum(int[] n, int t) {
         for (int i = 1; i < n.length; i++) {
             for (int j = i; j < n.length; j++) {
-                if (n[j] + n[j - 1] == t) {
+                if (n[j] + n[j - i] == t) {
                     return new int[] { j, j - i };
                 }
             }
         }
+        return new int[]{};
+    }
+}
