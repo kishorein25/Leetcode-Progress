6f970491dba9ad108a4c8f8d8a254ae5171d825e
Kishore S <ks6700822@gmail.com>
2026-09-25 23:16:59 +0530

    Solve Search in Rotated Sorted Array II (Medium) - search-in-rotated-sorted-array-ii

--- LeetCode/Medium/search-in-rotated-sorted-array-ii/Solution.java
+++ LeetCode/Medium/search-in-rotated-sorted-array-ii/Solution.java
@@ -1,17 +1,11 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]==target){
                 return true;
-            if (nums[mid] == t) {
-
-            mid = l + (r - l / 2);
-            }
-
-            else if(nums[mid] < t){
-                l=mid+1;
-            }
-
-
-        while (l < r) {
-            else {
-                r = mid-1;
             }
         }
-
+        return false;
+    }
+}
