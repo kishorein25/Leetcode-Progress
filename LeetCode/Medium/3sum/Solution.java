87e34935e50eb48946461cf06a8dc24d42ec96dc
Kishore S <ks6700822@gmail.com>
2026-09-26 12:55:02 +0530

    Solve 3Sum (Medium) - 3sum

---
+++ LeetCode/Medium/3sum/Solution.java
@@ -0,0 +1,9 @@
+        Set<List<Integer>> output = new HashSet<>();
+
+        int r = nums.length-1;
+        int l = 0;
+        int target = 0;
+        
+    public List<List<Integer>> threeSum(int[] nums) {
+class Solution {
+        Arrays.sort(nums);
