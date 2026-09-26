70b11a35fb297d0c779e46075b9cdd51850eab28
Kishore S <ks6700822@gmail.com>
2026-09-26 18:49:50 +0530

    Solve Maximum Product Subarray (Medium) - maximum-product-subarray

--- LeetCode/Medium/maximum-product-subarray/Solution.java
+++ LeetCode/Medium/maximum-product-subarray/Solution.java
@@ -1,6 +1,12 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int maxv = Integer.MIN_VALUE;
+        int maxv = 0;
         for(int i=1;i<nums.length;i++){
             int product = nums[i] * nums[i-1];
             if(product > maxv){
+                maxv= product;
+            }
+        }
+        return maxv;
+    }
+}
