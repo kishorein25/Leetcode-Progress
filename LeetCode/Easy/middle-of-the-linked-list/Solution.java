1b7c6a442c0b971e6415824a8f9464bf0b69acc6
Kishore S <ks6700822@gmail.com>
2026-09-26 19:39:29 +0530

    Solve Middle of the Linked List (Easy) - middle-of-the-linked-list

---
+++ LeetCode/Easy/middle-of-the-linked-list/Solution.java
@@ -0,0 +1,7 @@
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
